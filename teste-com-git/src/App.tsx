f2f952853b07c4e0a1b34be62aad93d8dedd21c9
andre-0303 <bandeiradev.ti@gmail.com>
2025-04-21 19:37:06 -0300

    feat: Adicionando roteamento

--- teste-com-git/src/App.tsx
+++ teste-com-git/src/App.tsx
@@ -1,14 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Esse é um projeto React + vite</h1>
-        <h2>Testando comandos git</h2>
-      </div>
-    </>
+    <BrowserRouter>
+      <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
+        <Link to="/">Home</Link>
+        <Link to="/sobre">Sobre</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
